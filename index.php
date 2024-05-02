ec50fc4b510930f99e66ec30884f403d5c520fb1
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-05-02 15:48:44 +0200

    completed parking filter (bonus 1)

--- index.php
+++ index.php
@@ -2,6 +2,15 @@
     include __DIR__ . "/Models/hotels.php";
     //var_dump($hotels);
 
+    if (!empty($_GET['stats']) || (isset($_GET['stats']) && $_GET['stats'] == 0)) {
+        $stats = $_GET['stats'];
+        $hotels = array_filter($hotels, function ($hotel) use ($stats) {
+          return $hotel['parking'] == $stats || $stats == "all";
+        });
+    } else {
+        $hotels = $hotels;
+    }
+
     include __DIR__ . "/Views/header.php";
 ?>
 
